59e08c7e64c47f2ffcb11e1908fd5ae0c27a728c
hknight <thale842@outlook.com>
2025-10-06 03:07:55 +0700

    fix user

--- docker.sh
+++ docker.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
+# ==========================================================
+# Chạy Docker Compose với UID/GID user hiện tại
+# ==========================================================
+
 # Load UID & GID của user hiện tại (vd: autechbox)
-export UID=$(id -u)
-export GID=$(id -g)
+export LOCAL_UID=$(id -u)
+export LOCAL_GID=$(id -g)
 
 # Load biến môi trường từ .env (nếu có)
 if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
-echo "Running Docker Compose with UID=$UID and GID=$GID..."
+echo "Running Docker Compose with LOCAL_UID=$LOCAL_UID and LOCAL_GID=$LOCAL_GID..."
 docker compose up -d
