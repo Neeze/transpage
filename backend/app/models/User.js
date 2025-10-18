6c89ddf836be18b288d4e2966c0d499849e61876
hknight <thale842@outlook.com>
2025-10-18 18:23:59 +0700

    fix default 5

--- backend/app/models/User.js
+++ backend/app/models/User.js
@@ -8,7 +8,7 @@ User.init({
     email: { type: DataTypes.STRING, unique: true, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: true }, // Google login có thể null
     role: { type: DataTypes.ENUM("user", "admin"), defaultValue: "user" },
-    points: { type: DataTypes.INTEGER, defaultValue: 0 },
+    points: { type: DataTypes.INTEGER, defaultValue: 5 },
     provider: { type: DataTypes.ENUM("local", "google"), defaultValue: "local" },
     googleId: { type: DataTypes.STRING, allowNull: true, unique: true },
     avatarUrl: { type: DataTypes.TEXT, allowNull: true }
